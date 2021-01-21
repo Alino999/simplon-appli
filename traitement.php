66a74e47b01fe0787581dee23ede657a3509908c
Aboubakary833 <aboubakarycisse472@gmail.com>
2021-01-21 14:14:02 +0100

    Optimisation du code

--- traitement.php
+++ traitement.php
@@ -14,7 +14,7 @@ $email = inputIsSet($_POST["email"]);
 
 //Manipulation des données $ville et $formation, $sexe
 $Villes = array('Ouaga' => 'Ouagadougou', 'Bobo' => 'Bobo Dioulasso');
-$Formations = array('Dev_web' => 'Developpement web et web mobile', 'Ref_dig' => 'Référent digital');
+$Formations = array('Dev_web' => 'Développement web et web mobile', 'Ref_dig' => 'Référent digital');
 $Sexe = array('homme' => 'Masculin', 'femme' => 'Feminin');
 function filterRelatedEl($el, $arr) {
     if($el != '') {
@@ -34,5 +34,4 @@ $sexe = filterRelatedEl($sexe, $Sexe);
 
 $preparation = $db->prepare("INSERT INTO apprenants (ID, Email, Nom, Prenom, Dates_de_naissance, Sexe, ville, formation) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
 $preparation->execute(array("", $email, $nom, $prenom, $naissance, $sexe, $ville, $formation));
-header('Location: formulaire.html');
 ?>
